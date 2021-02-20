f40e84a6bd9a49aefc78fef404336a59b76d6bb6
Brent Knudsen <brent@brents-mbp.lan>
2021-02-19 23:54:02 -0700

    changed footer title and changed header text size in page body

--- myjs/main.js
+++ myjs/main.js
@@ -2,3 +2,25 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+  // 1a - create lists
+
+  // 1b- name lists
+
+  // 2 - create limitless tasks
+
+  // 3 - mark task complete
+
+  // 4 - clear completed task
+
+  // 5 - edit task
+
+  // 6a - delete tasks without completing them first
+
+  // 6b - delete lists without completing them first
+
+  // 7a - use local storage to store list names
+
+  // 7b - use local storage to store tasks in each list
+
+  // 7c - use local storage to store login info - name, password
