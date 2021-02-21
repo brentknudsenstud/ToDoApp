96b0227781dd9774f1698e646f841c8e54fa77c2
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-02-20 19:51:32 -0700

    added basic structure in javascript file for iterating and getting Element By Ids for lists and tasks

--- myjs/main.js
+++ myjs/main.js
@@ -2,6 +2,67 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+// objects
+
+const lists = {
+  1: {name: 'Shopping list'},
+  2: {name: 'Honey do list'},
+  // extra lists below
+}
+
+const currentList = {
+  name: 'Shopping List',
+  todos: [
+
+  ]
+}
+
+todos: [
+  {text: 'bananas',
+  completed: false},
+  {text: '1 lb ground turkey',
+  completed: false}
+]
+
+const lists = {
+  1: {
+    name: 'Shopping List',
+    todos: [
+      {text: 'bananas',
+      completed: false,},
+      {
+        text: '1 lb ground turkey',
+        completed: false,
+      }
+    ]
+  },
+}
+ 
+const currentList = lists[0];
+
+// Insert sample function code from slide 14 below
+function show() {
+  //hold the html that will be displayed in the list sidebar
+  let listsHtml = '<div id="what-list" class="collection">';
+  // iterate through the lists to get their names
+  lists.forEach((list) => {
+    listsHtml += '<a id="list-name1" href="#!" class="collection-item detail">${list.name}</a>';
+  });
+  //print out the lists
+  document.getElementById('what-list').innerHTML = listsHtml;
+
+  //print out the name of the current list
+  document.getElementById(''). innerText = currentList.name;
+
+  // iterate over the tasks in the current list
+  let tasksHtml = '<div id="what-task" class="collection">';
+  currentList.tasks.forEach((list) => {
+    tasksHtml +='<a id="?" href="#!" class="collection-item">${tasks.text}</a>';
+  });
+
+  //print out the tasks
+  document.getElementById('what-task').innerHTML = tasksHtml;
+}
 
   // 1a - create lists
 
