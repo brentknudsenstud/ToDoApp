f9472847bed8e796c384061999caf8feba29a501
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-02-24 10:53:20 -0700

    changes and code organization that Mario helped me with

--- myjs/main.js
+++ myjs/main.js
@@ -18,30 +18,60 @@
 // }
 
 const tasks = [
-  {text: 'bananas',
-  completed: false},
-  {text: '1 lb ground turkey',
-  completed: false}
+  {
+    text: 'bananas',
+    completed: false
+  },
+  {
+    text: '1 lb ground turkey',
+    completed: false
+  }
 ]
 
-const lists = {
-  1: {
+const lists = [
+  {
     name: 'Shopping List',
     lists: [
-      {text: 'bananas',
-      completed: false,},
+      {
+        text: 'bananas',
+        completed: false,
+      },
       {
         text: '1 lb ground turkey',
         completed: false,
       }
     ]
   },
-}
- 
+]
+
 const currentList = lists[0];
 const currentTask = tasks[0];
 
-// Insert sample function code from slide 14 below
+class Task {
+  constructor(text, id) {
+    this.id = id;
+    this.text = text;
+    this.completed = false;
+  }
+  setCompleted() { }
+  editTasks() { }
+}
+
+class List {
+  constructor(
+    id,
+    name
+    ) { }
+  addTask() {
+
+  }
+  removeTask() {
+
+  }
+
+}
+let list = new List(1, 'hardware store') 
+
 function showList() {
   //hold the html that will be displayed in the list sidebar
   let listsHtml = '<div id="what-list" class="collection">';
@@ -51,46 +81,46 @@ function showList() {
     listsHtml += '<a id="${list.id}" href="#" class="collection-item detail">${list.name}</a>';
   });
 }
-  //print out the lists
-  document.getElementById('what-list').innerHTML = listsHtml;
+//print out the lists
+document.getElementById('what-list').innerHTML = listsHtml;
 
 function showTask() {
-// hold the html that will displayed in the task 'sidebar'
-let tasksHtml = '<div id="add-task" class="collection">';
-// iterate through the tasks to get their names
-tasks.forEach((task) => {
-// Make it so each task that is created has its own id as it loops through below.
-tasksHtml += '<a id="${task.id}" href="#" class="collection-item detail">${task.name}</a>';
-});
-// print out the tasks
-document.getElementById('add-task').innerHTML = tasksHtml;
+  // hold the html that will displayed in the task 'sidebar'
+  let tasksHtml = '<div id="add-task" class="collection">';
+  // iterate through the tasks to get their names
+  tasks.forEach((task) => {
+    // Make it so each task that is created has its own id as it loops through below.
+    tasksHtml += '<a id="${task.id}" href="#" class="collection-item detail">${task.name}</a>';
+  });
+  // print out the tasks
+  document.getElementById('add-task').innerHTML = tasksHtml;
   //print out the name of the current list
-  document.getElementById('selected-list-output'). innerText = currentList.name;
+  document.getElementById('selected-list-output').innerText = currentList.name;
 
   // iterate over the tasks in the current list
   let tasksHtml = '<div id="what-task" class="collection">';
   currentList.tasks.forEach((list) => {
-    tasksHtml +='<a id="?" href="#!" class="collection-item">${tasks.text}</a>';
+    tasksHtml += '<a id="?" href="#!" class="collection-item">${tasks.text}</a>';
   });
 }
 
 function addTasks() {
-// get the task from the task input box
-const text = 
-document.getElementById('enter-task').value;
-if(text) {
-  currentList.tasks.push({
-    text: text,
-    completed: false
-  })
-  showTask();
+  // get the task from the task input box
+  const text =
+    document.getElementById('enter-task').value;
+  if (text) {
+    currentList.tasks.push({
+      text: text,
+      completed: false
+    })
+    showTask();
+  }
 }
-}
-  // 1a, 1b - create lists and name lists
+// 1a, 1b - create lists and name lists
 function addLists() {
   const text =
-  document.getElementById('what-list').value;
-  if(text) {
+    document.getElementById('what-list').value;
+  if (text) {
     currentList.lists.push({
       text: text,
       completed: false,
@@ -103,25 +133,30 @@ function addLists() {
 function removeLists() {
 
 }
-  // 2 - create limitless tasks
+// 2 - create limitless tasks
 
-  // 3 - mark task complete
+// 3 - mark task complete
 function markTasksCompleted() {
 
 }
-  // 4 - clear completed task
+// 4 - clear completed task
 function clearCompletedTasks() {
 
 }
-  // 5 - edit task
+// 5 - edit task
 function editTasks() {
 
 }
-  // 6a - delete tasks without completing them first
+// 6a - delete tasks without completing them first
+// How will I let the user do that? Create a button for the user to do that. Have a remove button for each line.
+function removeTask(id) {
+  list.removeTask(id);
+  print();
+}
   // 6b - delete lists without completing them first
 
   // 7a - use local storage to store list names
 
   // 7b - use local storage to store tasks in each list
 
-  // 7c - use local storage to store login info - name, password
+  // 7c - use local storage to store login info 
