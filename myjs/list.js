9d5a495da1cec6ef4733e1a03388ee004bb82465
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-02-25 07:43:54 -0700

    minor changes

--- myjs/list.js
+++ myjs/list.js
@@ -3,26 +3,29 @@
 // 1c - remove lists
 
 class List {
-    constructor(
-      id,
-      name
-      ) { }
+    constructor(id,name) { 
+      this.id = id;
+      this.name = name;
+    }
     addTask() {
   
     }
     removeTask() {
   
     }
+    getTask() {
+
+    }
   
   }
   let list = new List(1, 'hardware store') 
 
   function showList() {
-    //hold the html that will be displayed in the list sidebar
+    // hold the html that will be displayed in the list sidebar
     let listsHtml = '<div id="what-list" class="collection">';
     // iterate through the lists to get their names
     lists.forEach((list) => {
-      //Make it so each list that is created has its own id as it loops through below.
+      // Make it so each list that is created has its own id as it loops through below.
       listsHtml += '<a id="${list.id}" href="#" class="collection-item detail">${list.name}</a>';
     });
   }
@@ -39,13 +42,30 @@ class List {
       })
       showList();
     }
+    window.localStorage.setItem(lists, JSON.stringify(text));
   }
 
+// 6b - delete lists without completing them first
   function removeLists() {
+    const text = 
+    document.getElementById('remove-list').value;
+    if (text) {
+      currentList.lists.pop({ // not sure if this is right
+        text: text,
+        completed: true,
+      })
+      showList();
+    }
 
 }
 
-// 6b - delete lists without completing them first
-
 // 7a - use local storage to store list names
+//localStorage can only store strings.
+// To store arrays or objects, you would have to convert them to strings 
+// To do this, use the JSON.stringify() method before passing to setItem()
+window.localStorage.setItem(listName, List);
+
+const list = {
+  name: ""
+}
   
