7f86af80e34c8eb2ca3484ea2ae98cc2f6c05ec9
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-03-10 11:18:32 -0700

    working on clearing completed tasks

--- myjs/task.js
+++ myjs/task.js
@@ -19,24 +19,25 @@ class Task {
   // Pattern function showTasks() below after the showLists() function in list.js
   function showTasks() {
     // hold the html that will be displayed in the task sidebar
-    let tasksHtml = '';
+    let tasksHtml = '<div id="task-container" class="col <!-- s12 m12 -->">';
     // iterate over the tasks in the current list
     tasks.forEach((task) => {
     // Make it so each task that is created has its own id as it loops through below
       tasksHtml += `
-      <div id="div-${task.id}" class="col s12 m12">
-           <div id="${task.id}" class="collection-item detail">
-           <label>
-           <input type="checkbox" />
-           <span>${task.text}</span>
-           <span><button class="btn" onclick="removeTask('${task.id}')">
-      <i class="far fa-trash-alt"></i>
-      </button><span>
-         </label>
-         </div>
-         <div class="divider"></div>
-      <div>`;
+      
+        <div id="${task.id}" class="collection-item detail">
+          <label>
+            <input type="checkbox" />
+            <span>${task.text}</span>
+            <span><button class="btn" onclick="removeTask('${task.id}')">
+              <i class="far fa-trash-alt"></i>
+            </button><span>
+          </label>
+        </div>
+         <div class="divider"></div>`;
     });
+    tasksHtml += '</div>'
+
     // print out the tasks
     document.getElementById('what-task').innerHTML = tasksHtml;
 
@@ -68,8 +69,23 @@ class Task {
 // 4 - clear completed task
 function clearCompletedTasks() {
 
-  /* create an eventListener or onclick event that responds to the tasks that are completed (checkbox present) so that the function below runs; make sure that it only responds to those task items that are checked, so I need to create an if/else statement to check for if true, then clear items, if false do not run clearCompletedTasks()that checks for checked items */
-  // Create a function that loops through the tasks and filters out the ones that are completed
+  /* create an eventListener or onclick event that responds to the tasks that are completed (checkbox present) so that the function below runs; make sure that it only responds to those task items that are checked that checks for checked items */
+  let taskComplete = [...document.querySelectorAll("#task-container input")]
+  .filter(function(atask) {
+      return atask.checked == true;
+  }); 
+  tasks = tasks.filter(function (atask) {
+  taskComplete;
+  if (taskComplete == false) {
+    // syntax inside parentheses for does the text exist in my task array
+  }
+});
+  showTasks();
+  console.log(taskComplete);
+  // Create a function that loops through the tasks and filters out the ones that are completed, use filter method
+  // filter: checked or not checked
+  // return true for any objects that have an unchecked box
+ 
 
 }
 // 5 - edit task
@@ -79,20 +95,5 @@ function editTasks() {
 
 
 // 6a - delete tasks without completing them first: done!
-// How will I let the user do that? Create a button for the user to do that. Have a remove button for each line.
-//  function removeTask(id, name) {
-//   list.removeTask(id, name);
-//   print();
-// }
-//let removeTasksHtml = '<div id="remove-task" class="collection">';
-// currentList.tasks.forEach((removetask) => {
-//   // Make it so each task that is created has its own id as it loops through below.
-//   removeTasksHtml += `<a id="${removetask.id}" href="#" class="collection-item detail">${removetask.name}</a>`;
-// });
-
-// document.getElementById("remove-task").innerHTML = removeTasksHtml
-
-
-
 // 7b - use local storage to store tasks in each list
 // Should I put this function or whatever it's called in the list.js file?
